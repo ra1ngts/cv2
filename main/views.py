25a18d324444838d614b5b5a8ca109286a48b17a
David Khurtsidze <ra1nygts@gmail.com>
2026-01-01 17:52:07 +0300

    Добавил данные для вывода в контекст

--- main/views.py
+++ main/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
+from django.utils.translation import gettext_lazy as _
+
+from main.models import Profile
+
+
 def index(request):
     context = {
-        'Title': 'About'
+        'title': _('Обо мне'),
+        'profile': Profile.get_profile_data()
     }
 
     return render(request, 'about.html', context=context)
