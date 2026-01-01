04fde9c092b07495bf61403dda3027be2d52f423
David Khurtsidze <ra1nygts@gmail.com>
2026-01-01 17:53:33 +0300

    Обновил модель Profile

--- main/models.py
+++ main/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import RegexValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
@@ -28,16 +29,22 @@ class BaseModelPublished(models.Model):
         abstract = True
 
 
-class Information(Basic):
+phone_validator = RegexValidator(
+    regex=r'^\+?1?\d{9,15}$',
+    message=_('Номер телефона должен быть в формате: "+79991234567". От 9 до 15 цифр.')
+)
+
+
+class Profile(Basic):
     name = models.CharField(
-        max_length=255,
-        help_text=_('Введите свое имя'),
+        max_length=30,
+        help_text=_('Введите имя'),
         verbose_name=_('Имя')
     )
     lastname = models.CharField(
         null=True,
         blank=True,
-        max_length=255,
+        max_length=50,
         help_text=_('Введите фамилию'),
         verbose_name=_('Фамилия')
     )
@@ -47,16 +54,24 @@ class Information(Basic):
         on_delete=models.SET_NULL,
         verbose_name=_('Изображение')
     )
+    occupation = models.CharField(
+        null=True,
+        blank=True,
+        max_length=255,
+        help_text=_('Введите название профессии'),
+        verbose_name=_('Профессия')
+    )
     description = models.TextField(
         null=True,
         blank=True,
-        help_text=_('Введите описание'),
+        help_text=_('Введите информацию о себе'),
         verbose_name=_('Описание')
     )
     phone = models.CharField(
         null=True,
         blank=True,
         max_length=15,
+        validators=[phone_validator],
         help_text=_('Введите номер телефона'),
         verbose_name=_('Номер телефона')
     )
@@ -66,11 +81,51 @@ class Information(Basic):
         max_length=40,
         help_text=_('Введите адрес вашей электронной почты'),
         verbose_name=_('Электронная почта')
-    ) #TODO добавить валидатор для email
+    )
+    whatsapp = models.URLField(
+        null=True,
+        blank=True,
+        max_length=500,
+        help_text=_('Введите ссылку на WhatsApp'),
+        verbose_name=_('WhatsApp')
+    )
+    telegram = models.URLField(
+        null=True,
+        blank=True,
+        max_length=500,
+        help_text=_('Введите ссылку на Telegram'),
+        verbose_name=_('Telegram')
+    )
+    linkedin = models.URLField(
+        null=True,
+        blank=True,
+        max_length=500,
+        help_text=_('Введите ссылку на LinkedIn'),
+        verbose_name=_('LinkedIn')
+    )
+    github = models.URLField(
+        null=True,
+        blank=True,
+        max_length=500,
+        help_text=_('Введите ссылку на GitHub'),
+        verbose_name=_('GitHub')
+    )
+    cv = models.URLField(
+        null=True,
+        blank=True,
+        max_length=500,
+        help_text = _('Введите ссылку на резюме'),
+        verbose_name=_('Ссылка на резюме')
+    )
 
     def __str__(self):
         return self.name
 
+    @classmethod
+    def get_profile_data(cls):
+        obj, created = cls.objects.get_or_create(pk=1)
+        return obj
+
     class Meta:
-        verbose_name = _('Информация')
-        verbose_name_plural = _('Информация')
+        verbose_name = _('Личная информация')
+        verbose_name_plural = _('Личная информация')
