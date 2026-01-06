b82c3d42741778f79d826e9ed9c8a63724b75a2c
David Khurtsidze <ra1nygts@gmail.com>
2026-01-06 12:03:01 +0300

    Добавил static и media и изменил директории для static и media

--- cv2/urls.py
+++ cv2/urls.py
@@ -14,11 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from main import urls as main_urls
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('about/', include('main.urls')),
+    path('', include(main_urls, namespace='main')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
