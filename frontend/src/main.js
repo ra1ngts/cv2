581367156330c9de7a1397b248db400edd802216
David Khurtsidze <ra1nygts@gmail.com>
2026-01-06 17:16:16 +0300

    App перенес в lib/components

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,6 @@
 import { mount } from 'svelte'
 import './app.css'
-import App from './App.svelte'
+import App from './lib/components/App.svelte'
 
 //Target
 const appTarget = document.getElementById('app');
