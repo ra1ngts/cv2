5dd060d46828883aea227b8ad65d4c79a00554a2
David Khurtsidze <ra1nygts@gmail.com>
2026-01-06 12:03:54 +0300

    Обновил views.py

--- main/views.py
+++ main/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
 from django.utils.translation import gettext_lazy as _
@@ -6,9 +7,12 @@ from main.models import Profile
 
 
 def index(request):
-    context = {
+    ctx = {
+        'status': 'success',
         'title': _('Обо мне'),
         'profile': Profile.get_profile_data()
     }
 
-    return render(request, 'about.html', context=context)
+    # return JsonResponse(ctx)
+
+    return render(request, 'about.html', context=ctx)
