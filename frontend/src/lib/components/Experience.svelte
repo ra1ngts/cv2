a19259aa8e4c1fde2905ac51ba092a47605910e8
David Khurtsidze <ra1nygts@gmail.com>
2026-01-07 18:58:46 +0300

    Добавил отображение скилов в опыте

--- frontend/src/lib/components/Experience.svelte
+++ frontend/src/lib/components/Experience.svelte
@@ -21,6 +21,25 @@
         <div class="text-sm">
           {field.achievements}
         </div>
+
+        {#if field.skills && field.skills.length > 0}
+          <div class="flex flex-row gap-4">
+            {#each field.skills as skill}
+              <div class="flex gap-2 items-center rounded-2xl border border-gray-800 text-gray-400 p-2">
+                {#if skill.image}
+                  <div>
+                    <img src={skill.image} alt={skill.name} class="w-10 h-10 object-cover" />
+                  </div>
+                {/if}
+
+                <div class="text-sm">
+                  {skill.name}
+                  <!-- {skill.level} -->
+                </div>
+              </div>
+            {/each}
+          </div>
+        {/if}
       </div>
     </div>
   </a>
