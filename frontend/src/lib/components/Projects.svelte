4b317998ebe9894c726d625c5782ea6ad6c6b793
David Khurtsidze <ra1nygts@gmail.com>
2026-01-07 19:01:04 +0300

    Добавил Projects.svelte

---
+++ frontend/src/lib/components/Projects.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { stateCtx } from '../../store.svelte';
+</script>
+
+<div>
+  {#if stateCtx.projects.length > 0}
+    {#each stateCtx.projects as item}
+      <p>{item.title} — {item.description}</p>
+    {/each}
+  {/if}
+</div>
