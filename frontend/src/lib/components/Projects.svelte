e09b7170f03186685b8e2d7bd3ac0ac97885d681
David Khurtsidze <ra1nygts@gmail.com>
2026-01-08 19:10:14 +0300

    Обновил Projects.svelte

--- frontend/src/lib/components/Projects.svelte
+++ frontend/src/lib/components/Projects.svelte
@@ -33,12 +33,12 @@
   <div class="swiper-wrapper">
     {#each stateCtx.projects as item}
       <div class="swiper-slide">
-        <div class="grid grid-cols-4 gap-2 px-10">
+        <div class="grid grid-cols-4 gap-4 px-10">
           <div class="col-span-1 h-[350px]">
             <img src={item.main_image} alt={item.title} class="w-full h-full object-cover rounded-2xl" />
           </div>
 
-          <div class="col-span-1 flex flex-col gap-2 h-[350px] overflow-hidden">
+          <div class="col-span-1 flex flex-col gap-4 h-[350px] overflow-hidden">
             {#each item.images.slice(0, 3) as img}
               <div class="flex-1 min-h-0">
                 <img src={img} alt={item.title} class="w-full h-full object-cover rounded-2xl" />
@@ -46,19 +46,20 @@
             {/each}
           </div>
 
-          <div class="col-span-2 flex flex-col h-[350px] overflow-hidden">
-            <h3 class="">{@html item.title}</h3>
-            <p class="flex-1 overflow-y-auto">{@html item.description}</p>
+          <div class="col-span-2 flex flex-col h-[350px]">
+            <h3 class="text-2xl font-bold pb-2">{@html item.title}</h3>
+            <div class="text-sm flex-1 overflow-y-auto">{@html item.description}</div>
 
             {#if item.technologies.length > 0}
-              <div class="flex gap-2">
-                {#each item.technologies as tech}
-                  <img
-                    src={tech.image}
-                    alt={tech.name}
-                    title={tech.name}
-                    class="w-6 h-6 grayscale hover:grayscale-0 transition-all"
-                  />
+              <div class="flex gap-2 items-center mt-4">
+                {#each item.technologies as skill}
+                  <div class="flex gap-2 items-center rounded-2xl px-4 py-1 bg-gray-900">
+                    <img src={skill.image} alt={skill.name} title={skill.name} class="flex w-10 h-10 object-cover" />
+
+                    <div class="text-sm font-semibold">
+                      {skill.name}
+                    </div>
+                  </div>
                 {/each}
               </div>
             {/if}
