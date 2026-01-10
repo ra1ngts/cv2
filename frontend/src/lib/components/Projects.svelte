e8ca1e90d18af56c921326f43717ff92d373255b
David Khurtsidze <ra1nygts@gmail.com>
2026-01-10 19:23:28 +0300

    Добавил стили

--- frontend/src/lib/components/Projects.svelte
+++ frontend/src/lib/components/Projects.svelte
@@ -19,7 +19,7 @@
       spaceBetween: 30,
       loop: false,
       pagination: {
-        el: '.swiper-pagination',
+        el: '.swiper-pagination-projects',
         clickable: true,
       },
       navigation: {
@@ -43,56 +43,65 @@
   });
 </script>
 
-<div class="swiper">
-  <div class="swiper-wrapper">
-    {#each stateCtx.projects as item}
-      <div class="swiper-slide">
-        <div class="grid grid-cols-{item.images.length === 0 ? '1' : '4'} gap-4 px-10">
-          {#if item.images.length > 0}
-            <div class="col-span-{item.images.length === 1 ? '2' : '1'} h-[350px]">
-              <a href={item.images[0]} data-fancybox="gallery-{item.id}">
-                <img src={item.images[0]} alt={item.title} class="w-full h-full object-cover rounded-2xl" />
-              </a>
-            </div>
-
-            {#if item.images.length > 1}
-              <div class="col-span-1 flex flex-col gap-4 h-[350px] overflow-hidden">
-                {#each item.images.slice(1, 4) as img}
-                  <div class="flex-1 min-h-0 f-carousel__slide">
-                    <a href={img} data-fancybox="gallery-{item.id}">
-                      <img src={img} alt={item.title} class="w-full h-full object-cover rounded-2xl" />
-                    </a>
-                  </div>
-                {/each}
+<div
+  class="projects pt-4 rounded-2xl transition-all duration-300 hover:bg-white/5 hover:backdrop-blur-md
+        hover:shadow-2xl hover:shadow-cyan-950/20 group block"
+>
+  <div class="swiper">
+    <div class="swiper-wrapper">
+      {#each stateCtx.projects as item}
+        <div class="swiper-slide">
+          <div class="grid grid-cols-{item.images.length === 0 ? '1' : '4'} gap-4 px-10">
+            {#if item.images.length > 0}
+              <div class="col-span-{item.images.length === 1 ? '2' : '1'} h-[350px]">
+                <a href={item.images[0]} data-fancybox="gallery-{item.id}">
+                  <img src={item.images[0]} alt={item.title} class="w-full h-full object-cover rounded-2xl" />
+                </a>
               </div>
-            {/if}
-          {/if}
 
-          <div class="col-span-2 flex flex-col h-[350px]">
-            <h3 class="text-2xl font-bold pb-2">{@html item.title}</h3>
-            <div class="text-sm flex-1 overflow-y-auto">{@html item.description}</div>
-
-            {#if item.technologies.length > 0}
-              <div class="flex flex-wrap gap-2 items-center mt-4">
-                {#each item.technologies as skill}
-                  <div class="flex gap-2 items-center rounded-2xl px-4 py-1 bg-gray-900">
-                    <img src={skill.image} alt={skill.name} title={skill.name} class="flex w-full h-6 object-cover" />
-
-                    <div class="text-sm font-semibold">
-                      {skill.name}
+              {#if item.images.length > 1}
+                <div class="col-span-1 flex flex-col gap-4 h-[350px] overflow-hidden">
+                  {#each item.images.slice(1, 4) as img}
+                    <div class="flex-1 min-h-0 f-carousel__slide">
+                      <a href={img} data-fancybox="gallery-{item.id}">
+                        <img src={img} alt={item.title} class="w-full h-full object-cover rounded-2xl" />
+                      </a>
                     </div>
-                  </div>
-                {/each}
-              </div>
+                  {/each}
+                </div>
+              {/if}
             {/if}
+
+            <div class="col-span-2 flex flex-col h-[350px]">
+              <h3 class="text-2xl font-bold pb-2 transition-colors duration-300 group-hover:text-cyan-200">
+                {@html item.title}
+              </h3>
+              <div class="text-sm flex-1 overflow-y-auto">{@html item.description}</div>
+
+              {#if item.technologies.length > 0}
+                <div class="flex flex-wrap gap-2 items-center mt-4">
+                  {#each item.technologies as skill}
+                    <div
+                      class="flex gap-2 items-center rounded-2xl px-4 py-1 group-hover:text-cyan-200 bg-gray-900 group-hover:bg-gray-800"
+                    >
+                      <img src={skill.image} alt={skill.name} title={skill.name} class="flex w-full h-6 object-cover" />
+
+                      <div class="text-sm font-semibold">
+                        {skill.name}
+                      </div>
+                    </div>
+                  {/each}
+                </div>
+              {/if}
+            </div>
           </div>
         </div>
-      </div>
-    {/each}
+      {/each}
+    </div>
+
+    <div class="swiper-button-prev"></div>
+    <div class="swiper-button-next"></div>
   </div>
 
-  <div class="swiper-pagination"></div>
-
-  <div class="swiper-button-prev"></div>
-  <div class="swiper-button-next"></div>
+  <div class="swiper-pagination-projects"></div>
 </div>
