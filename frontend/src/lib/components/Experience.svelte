9a4ca4e5681f56f499af0baebf6b92cf2f7efd1f
David Khurtsidze <ra1nygts@gmail.com>
2026-01-17 19:03:29 +0300

    Обновил Experience.svelte

--- frontend/src/lib/components/Experience.svelte
+++ frontend/src/lib/components/Experience.svelte
@@ -4,33 +4,33 @@
 </script>
 
 {#if stateCtx.experience.length > 0}
-  {#each stateCtx.experience as field}
-    <a href={field.company_url} target="_blank" rel="noopener noreferrer" class="group block">
+  {#each stateCtx.experience as entry}
+    <a href={entry.company_url} target="_blank" rel="noopener noreferrer" class="group block">
       <div
         class="grid grid-cols-2 gap-4 p-4 rounded-2xl transition-all duration-300 hover:bg-white/5 hover:backdrop-blur-md
         hover:shadow-2xl hover:shadow-cyan-950/20"
       >
         <div class="text-gray-400">
-          {getDuration(field.start_date, field.end_date, field.is_current)}
+          {getDuration(entry.start_date, entry.end_date, entry.is_current)}
         </div>
 
         <div class="grid grid-cols-1 gap-4">
           <div class="text-gray-300 font-semibold text-lg transition-colors duration-300 group-hover:text-cyan-200">
-            {field.position}, {field.company}
+            {entry.position}, {entry.company}
           </div>
 
           <div class="text-sm">
-            {field.achievements}
+            {entry.achievements}
           </div>
 
-          {#if field.skills && field.skills.length > 0}
-            <div class="flex flex-row gap-4">
-              {#each field.skills as skill}
+          {#if entry.skills && entry.skills.length > 0}
+            <div class="flex flex-wrap flex-row gap-4">
+              {#each entry.skills as skill}
                 <div
-                  class="flex gap-2 items-center rounded-2xl px-4 py-2 group-hover:text-cyan-200 bg-gray-900 group-hover:bg-gray-800"
+                  class="flex gap-2 items-center rounded-2xl px-4 py-1 group-hover:text-cyan-200 bg-gray-900 group-hover:bg-gray-800"
                 >
                   {#if skill.image}
-                    <img src={skill.image} alt={skill.name} class="w-10 h-10 object-cover" />
+                    <img src={skill.image} alt={skill.name} class="w-10 h-10 object-contain" />
                   {/if}
 
                   <div class="text-sm font-semibold">
@@ -45,5 +45,5 @@
     </a>
   {/each}
 {:else}
-  <p>No data available</p>
+  <p>No entries available</p>
 {/if}
