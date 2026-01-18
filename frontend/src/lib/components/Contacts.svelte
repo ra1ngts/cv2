a0e8c5db5ccb00ebd6dcd373e2d17d47defbb4f7
David Khurtsidze <ra1nygts@gmail.com>
2026-01-18 18:58:23 +0300

    Добавил временный рендеринг формы

--- frontend/src/lib/components/Contacts.svelte
+++ frontend/src/lib/components/Contacts.svelte
@@ -1,9 +1,72 @@
 <script>
-  import { stateCtx } from '../../store.svelte';
+  import { stateCtx, contactsForm } from '../../store.svelte';
+
+  const fieldOrder = ['name', 'email', 'subject', 'message'];
+
+  console.log(document.cookie.match(/csrftoken=([^;]+)/)?.[1]);
+
+  async function handleSend() {
+    const body = contactsForm();
+
+    // В 2026 году Fetch — база. Не забудь про CSRF токен из кук!
+    const response = await fetch('/', {
+      method: 'POST',
+      //   body: body,
+      headers: {
+        Accept: 'application/json',
+        'X-CSRFToken': document.cookie.match(/csrftoken=([^;]+)/)?.[1],
+      },
+    });
+
+    const result = await response.json();
+    if (result.status === 'success') {
+      alert('Улетело!');
+    }
+  }
 </script>
 
 <div>
-  {#each Object.entries(stateCtx.form) as [k, v]}
-    {k} {v}
-  {/each}
+  <div class="contact-form">
+    {#each fieldOrder as key}
+      <!-- Берем конфиг поля из stateCtx.form (то, что прислал Django) -->
+      {#if stateCtx.form[key]}
+        {@const fieldConfig = stateCtx.form[key]}
+
+        <div class="field-group">
+          <label for={key}>{fieldConfig.label}</label>
+
+          {#if key === 'subject'}
+            <!-- Привязываем значение к contactsData -->
+            <select id={key} bind:value={stateCtx.contactsData[key]}>
+              <option value="">Выберите тему</option>
+              {#each fieldConfig.choices as [val, label]}
+                <option value={val}>{label}</option>
+              {/each}
+            </select>
+          {:else if fieldConfig.input_type === 'textarea'}
+            <textarea id={key} bind:value={stateCtx.contactsData[key]}></textarea>
+          {:else}
+            <input type={fieldConfig.input_type} id={key} bind:value={stateCtx.contactsData[key]} />
+          {/if}
+        </div>
+      {/if}
+    {/each}
+
+    <button onclick={handleSend}> Отправить </button>
+  </div>
 </div>
+
+<style>
+  .field-group {
+    margin-bottom: 1rem;
+    display: flex;
+    flex-direction: column;
+  }
+  label {
+    font-weight: bold;
+    margin-bottom: 0.3rem;
+  }
+  textarea {
+    min-height: 100px;
+  }
+</style>
