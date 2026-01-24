e31ab6c2ce7d62dab2f9468500167ea17d3eb4a9
David Khurtsidze <ra1nygts@gmail.com>
2026-01-24 15:41:09 +0300

    Убрал выбор заголовков из темы

--- main/forms.py
+++ main/forms.py
@@ -3,21 +3,14 @@ from django.forms.widgets import EmailInput
 from django.utils.translation import gettext_lazy as _
 
 class ContactsForm(forms.Form):
-    SUBJECT_CHOICES = [
-        ('', _('Выберите тему')),
-        ('work', _('Предложение работы')),
-        ('project', _('Фриланс / Проект')),
-        ('question', _('Вопрос'))
-    ]
-
     name = forms.CharField(
         label=_('Имя'),
         max_length=100,
         widget=forms.TextInput(attrs={'placeholder': _('Ваше имя')})
     )
-    subject = forms.ChoiceField(
+    subject = forms.CharField(
         label=_('Тема'),
-        choices=SUBJECT_CHOICES,
+        max_length=200,
         required=True
     )
     message = forms.CharField(
