0d0390f8522e9fe75bd12b9076c5fdb7fc1cee50
David Khurtsidze <ra1nygts@gmail.com>
2026-01-31 11:52:56 +0300

    Добавил recaptcha_token в форму

--- frontend/src/store.svelte.js
+++ frontend/src/store.svelte.js
@@ -32,12 +32,17 @@ export const stateCtx = $state({
     ]
 })
 
-export const contactsForm = () => {
+export const contactsForm = (recaptcha_token = null) => {
   const form = new FormData();
   form.set('name', stateCtx.contactsData.name || '');
   form.set('subject', stateCtx.contactsData.subject || '');
   form.set('message', stateCtx.contactsData.message || '');
   form.set('email', stateCtx.contactsData.email || '');
+
+  if (recaptcha_token) {
+    form.set('recaptcha_token', recaptcha_token);
+  }
+
   console.log('contactsForm has sent data:', Object.fromEntries(form.entries()));
   return form;
 };
