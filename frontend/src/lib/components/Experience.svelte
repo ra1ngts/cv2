bd6bbbe648163522885eb1ed997490d05b5cb8d2
David Khurtsidze <ra1nygts@gmail.com>
2026-02-01 18:07:43 +0300

    Добавил иконку в Experience.svelte

--- frontend/src/lib/components/Experience.svelte
+++ frontend/src/lib/components/Experience.svelte
@@ -10,7 +10,15 @@
         class="grid sm:grid-cols-3 grid-cols-1 sm:gap-4 gap-2 p-2 md:p-4 rounded-2xl transition-all duration-300 hover:bg-white/5 hover:backdrop-blur-md
         hover:shadow-2xl hover:shadow-cyan-950/20"
       >
-        <div class="col-span-1">
+        <div class="col-span-1 flex items-center gap-2 [&_svg]:fill-gray-400">
+          <div>
+            <svg class="w-full h-6 object-cover" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"
+              ><path
+                d="M120 0c13.3 0 24 10.7 24 24l0 40 160 0 0-40c0-13.3 10.7-24 24-24s24 10.7 24 24l0 40 32 0c35.3 0 64 28.7 64 64l0 288c0 35.3-28.7 64-64 64L64 480c-35.3 0-64-28.7-64-64L0 128C0 92.7 28.7 64 64 64l32 0 0-40c0-13.3 10.7-24 24-24zm0 112l-56 0c-8.8 0-16 7.2-16 16l0 48 352 0 0-48c0-8.8-7.2-16-16-16l-264 0zM48 224l0 192c0 8.8 7.2 16 16 16l320 0c8.8 0 16-7.2 16-16l0-192-352 0z"
+              /></svg
+            >
+          </div>
+
           <div class="text-gray-400">
             {getDuration(entry.start_date, entry.end_date, entry.is_current)}
           </div>
