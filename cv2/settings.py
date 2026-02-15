e087403d20de65a08d03cbdb13a269bbf85c8087
David Khurtsidze <ra1nygts@gmail.com>
2026-02-15 19:18:33 +0300

    Изменил дефолтный язык

--- cv2/settings.py
+++ cv2/settings.py
@@ -116,7 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'ru'
+LANGUAGE_CODE = 'en-us'
 
 LANGUAGES = [
     ('ru', _('Russian')),
