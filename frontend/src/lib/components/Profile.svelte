876d84dcd9b09128929bac51cb11646aa2c87834
David Khurtsidze <ra1nygts@gmail.com>
2026-02-28 18:59:08 +0300

    Подобрал размеры иконок и эффектов для > lg

--- frontend/src/lib/components/Profile.svelte
+++ frontend/src/lib/components/Profile.svelte
@@ -44,7 +44,7 @@
           slidesPerView: 2,
           spaceBetween: 15,
           navigation: {
-            enabled: true,
+            enabled: false,
           },
           pagination: {
             enabled: true,
@@ -301,8 +301,8 @@
         {#each stateCtx.certificates as skill}
           <div class="swiper-slide h-auto! flex">
             <div
-              class="group h-full flex flex-col gap-4 px-0 lg:px-10 py-4 rounded-2xl transition-all duration-300 hover:bg-white/5 hover:backdrop-blur-md
-                hover:shadow-2xl hover:shadow-cyan-950/30"
+              class="group h-full flex flex-col gap-4 px-4 lg:px-10 py-4 rounded-2xl transition-all duration-300 bg-white/5 shadow-2xl backdrop-blur-md hover:bg-white/5 lg:bg-transparent lg:hover:backdrop-blur-md
+                lg:hover:shadow-2xl lg:hover:shadow-cyan-950/20"
             >
               <div class="h-64 overflow-hidden rounded-2xl bg-white">
                 <a href={skill.image} data-fancybox="skills-gallery-{skill.id}">
@@ -315,27 +315,40 @@
               </div>
 
               <div
-                class="flex flex-row gap-4 items-center transition-colors duration-300 group-hover:text-cyan-200 {skill.skill_url
-                  ? 'cursor-pointer'
-                  : ''}"
+                class="grid grid-cols-[1fr_auto_auto] py-2 [&_svg]:h-6 [&_svg]:w-6 sm:[&_svg]:h-7 sm:[&_svg]:w-7 [&_svg]:fill-cyan-200 lg:[&_svg]:fill-gray-500 lg:[&_svg]:hover:fill-cyan-200"
               >
                 {#if skill.skill_url}
-                  <a
-                    href={skill.skill_url}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    aria-label={skill.skill_url}
-                    class="flex w-full justify-between"
+                  <div
+                    class="sm:text-md font-bold transition-colors duration-300 text-cyan-200 lg:text-gray-500 lg:group-hover:text-cyan-200"
                   >
-                    <div class="text-md pr-2">{skill.name}</div>
-                    <svg class="h-6 w-6 shrink-0 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"
-                      ><path
-                        d="M451.5 160C434.9 160 418.8 164.5 404.7 172.7C388.9 156.7 370.5 143.3 350.2 133.2C378.4 109.2 414.3 96 451.5 96C537.9 96 608 166 608 252.5C608 294 591.5 333.8 562.2 363.1L491.1 434.2C461.8 463.5 422 480 380.5 480C294.1 480 224 410 224 323.5C224 322 224 320.5 224.1 319C224.6 301.3 239.3 287.4 257 287.9C274.7 288.4 288.6 303.1 288.1 320.8C288.1 321.7 288.1 322.6 288.1 323.4C288.1 374.5 329.5 415.9 380.6 415.9C405.1 415.9 428.6 406.2 446 388.8L517.1 317.7C534.4 300.4 544.2 276.8 544.2 252.3C544.2 201.2 502.8 159.8 451.7 159.8zM307.2 237.3C305.3 236.5 303.4 235.4 301.7 234.2C289.1 227.7 274.7 224 259.6 224C235.1 224 211.6 233.7 194.2 251.1L123.1 322.2C105.8 339.5 96 363.1 96 387.6C96 438.7 137.4 480.1 188.5 480.1C205 480.1 221.1 475.7 235.2 467.5C251 483.5 269.4 496.9 289.8 507C261.6 530.9 225.8 544.2 188.5 544.2C102.1 544.2 32 474.2 32 387.7C32 346.2 48.5 306.4 77.8 277.1L148.9 206C178.2 176.7 218 160.2 259.5 160.2C346.1 160.2 416 230.8 416 317.1C416 318.4 416 319.7 416 321C415.6 338.7 400.9 352.6 383.2 352.2C365.5 351.8 351.6 337.1 352 319.4C352 318.6 352 317.9 352 317.1C352 283.4 334 253.8 307.2 237.5z"
-                      />
-                    </svg>
-                  </a>
+                    {skill.name}
+                  </div>
+                  <div class="ps-2">
+                    <a
+                      href={skill.skill_url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      aria-label={skill.skill_url}
+                      class="flex w-full justify-between items-center group/link"
+                    >
+                      <svg
+                        class="shrink-0 h-6 w-6 sm:h-7 sm:w-7 transition-colors duration-300 fill-current
+                      text-cyan-200 lg:text-gray-500 lg:group-hover/link:text-cyan-200"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 640 640"
+                      >
+                        <path
+                          d="M451.5 160C434.9 160 418.8 164.5 404.7 172.7C388.9 156.7 370.5 143.3 350.2 133.2C378.4 109.2 414.3 96 451.5 96C537.9 96 608 166 608 252.5C608 294 591.5 333.8 562.2 363.1L491.1 434.2C461.8 463.5 422 480 380.5 480C294.1 480 224 410 224 323.5C224 322 224 320.5 224.1 319C224.6 301.3 239.3 287.4 257 287.9C274.7 288.4 288.6 303.1 288.1 320.8C288.1 321.7 288.1 322.6 288.1 323.4C288.1 374.5 329.5 415.9 380.6 415.9C405.1 415.9 428.6 406.2 446 388.8L517.1 317.7C534.4 300.4 544.2 276.8 544.2 252.3C544.2 201.2 502.8 159.8 451.7 159.8zM307.2 237.3C305.3 236.5 303.4 235.4 301.7 234.2C289.1 227.7 274.7 224 259.6 224C235.1 224 211.6 233.7 194.2 251.1L123.1 322.2C105.8 339.5 96 363.1 96 387.6C96 438.7 137.4 480.1 188.5 480.1C205 480.1 221.1 475.7 235.2 467.5C251 483.5 269.4 496.9 289.8 507C261.6 530.9 225.8 544.2 188.5 544.2C102.1 544.2 32 474.2 32 387.7C32 346.2 48.5 306.4 77.8 277.1L148.9 206C178.2 176.7 218 160.2 259.5 160.2C346.1 160.2 416 230.8 416 317.1C416 318.4 416 319.7 416 321C415.6 338.7 400.9 352.6 383.2 352.2C365.5 351.8 351.6 337.1 352 319.4C352 318.6 352 317.9 352 317.1C352 283.4 334 253.8 307.2 237.5z"
+                        />
+                      </svg>
+                    </a>
+                  </div>
                 {:else}
-                  <div class="text-md justify-center">{skill.name}</div>
+                  <div
+                    class="text-md justify-center transition-colors duration-300 text-cyan-200 lg:text-gray-500 lg:group-hover:text-cyan-200"
+                  >
+                    {skill.name}
+                  </div>
                 {/if}
               </div>
             </div>
